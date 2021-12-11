ed54a0dd276d8fbb2feea9ade00a6f50298d50a1
Karim-W <Karim.wael@gmail.com>
2021-12-11 12:29:46 +0400

    works with time remaining

--- Prayer/Classes/Prayer.swift
+++ Prayer/Classes/Prayer.swift
@@ -12,10 +12,31 @@ class Prayer:Identifiable{
     private var _prayerName:prayerType!
     private var _startTime:String!
     private var _timePrayed:String!
+    private var name:String!
     init(pName:prayerType,pStart:String){
         self._prayerName = pName
         self._startTime = pStart
+        self.name = setPrayerName(type:pName)
     }
+    
+    func setPrayerName(type:prayerType)-> String{
+        switch type {
+        case .Fajr:
+            return "Fajr"
+        case .Duhur:
+            return "Duhur"
+        case .Asr:
+            return "Asr"
+        case .Maghrib:
+            return "Maghrib"
+        case .Isha:
+            return "Isha"
+        }
+    }
+    func getPrayerName()->String{
+        return self.name
+    }
+    
     var startTime:String{
         set{_startTime=newValue}
         get{return _startTime}
--- Prayer/ContentView.swift
+++ Prayer/ContentView.swift
@@ -11,29 +11,39 @@ import CoreData
 struct ContentView: View {
     @State var loading = true
     @State var dayInstance = Day()
+    @State var nextPrayer:String = ""
+    @State var currentPrayer:String = ""
+    @State var TimeUntil:String = ""
+    @State var nextTime:String = ""
+    @State var remaining:String = ""
+    let timer = Timer.publish(every: 60, on: .main, in: .common).autoconnect()
+    //    let currentTime = getFormattedDate(date: Date(), format: "HH:mm")
     var body: some View {
         VStack{
             if(!loading){
-            HStack{
-                ForEach(dayInstance.getPrayerTimes()) { aPrayer in
-                    VStack{
-//                        Text(aPrayer.prayerName)
-                        Text(aPrayer.startTime)
-                    }.padding()
+                HStack{
+                    ForEach(dayInstance.getPrayerTimes()) { aPrayer in
+                        VStack{
+                            Text(aPrayer.getPrayerName())
+                            Text(aPrayer.startTime)
+                        }.padding()
+                    }
                 }
-                
-                
-            }
+                Text("Time Remaining: \(remaining)").padding().onReceive(timer) { time in
+                    setTimeRemaining()
+                }
             }else{
                 VStack{
                     ProgressView().progressViewStyle(.circular)
                     Text("Loading Prayers")
                 }
             }
-            Button("pwess") {
+            VStack{
+                Text("Current Prayer: \(currentPrayer)")
+                Text("Next Prayer: \(nextPrayer)")
+            }.onReceive(timer) { time in
                 getBearing()
             }
-            
         }.padding().frame(minWidth: 400).onAppear {
             initialLoad()
         }
@@ -49,22 +59,58 @@ struct ContentView: View {
                 prayers.append(Prayer(pName: prayerType.Maghrib, pStart: PrayerAPIResponse.maghrib))
                 prayers.append(Prayer(pName: prayerType.Isha, pStart: PrayerAPIResponse.isha))
                 dayInstance.setPrayersOfTheDay(prayerArr: prayers)
+                getBearing()
+                setTimeRemaining()
                 loading = false
             })
         }
     }
     func getBearing(){
-        let formatingDate = getFormattedDate(date: Date(), format: "HH:MM")
-//        for timing in prayerTimings{
-//            print(timing.key,":",timing.value)
-//        }
-                print(formatingDate)
+        let formatingDate = getFormattedDate(date: Date(), format: "HH:mm")
+        let prs = dayInstance.getPrayerTimes()
+        for i in 0...4{
+            if(prs[i].startTime>formatingDate){
+                currentPrayer = prs[i-1].getPrayerName()
+                nextPrayer = prs[i].getPrayerName()
+                nextTime = prs[i].startTime
+                
+                return
+            }
+        }
+        
+        print(formatingDate)
     }
     func getFormattedDate(date: Date, format: String) -> String {
-            let dateformat = DateFormatter()
-            dateformat.dateFormat = format
-            return dateformat.string(from: date)
+        let dateformat = DateFormatter()
+        dateformat.dateFormat = format
+        return dateformat.string(from: date)
     }
+    
+    //function to update every minute
+    func updateRemainingTime(){
+        //        Timer.scheduledTimer(timeInterval: 60, target: Any, selector: setTimeRemaining, userInfo: Any?, repeats: true)
+    }
+    func setTimeRemaining(){
+        remaining = getTimeDiffrence(start: getFormattedDate(date: Date(), format: "HH:mm"), end: nextTime)
+    }
+    //Function to get the time diffrence
+    func getTimeDiffrence(start: String, end: String) -> String{
+        print("start",start)
+        print("end",end)
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "HH:mm"
+        let startDate = dateFormatter.date(from: start)
+        let endDate = dateFormatter.date(from: end)
+        let calendar = Calendar.current
+        let components = calendar.dateComponents([.hour, .minute], from: startDate!, to: endDate!)
+        if(components.hour! > 0){
+            return "\(components.hour!) H: \(components.minute!) M"
+        }else{
+            return "\(components.minute!) minutes"
+        }
+    }
+    
+    
 }
 
 
